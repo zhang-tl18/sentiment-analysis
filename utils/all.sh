326009ee75ac3e7b5dffc568883b72868508713d
Tianle Zhang <2446588658@qq.com>
2022-08-19 16:23:34 +0800

    update

--- utils/all.sh
+++
@@ -1,7 +0,0 @@
-# conda activate ai
-
-python ../main.py --model MLP --train --epochs 40 --patience 7
-python ../main.py --model CNN --train --epochs 40 --patience 7
-python ../main.py --model RNN --train --epochs 40 --patience 7
-
-python ../main.py --report
